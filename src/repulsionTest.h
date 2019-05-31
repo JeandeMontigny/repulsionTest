652d0fdf5ea83974e59d470e227c53c5d99de443
JeandeMontigny <J.De-Montigny2@newcastle.ac.uk>
2019-05-31 10:05:46 +0100

    initial commit

--- src/repulsionTest.h
+++ src/repulsionTest.h
@@ -21,15 +21,33 @@ namespace bdm {
 // Define compile time parameter
 BDM_CTPARAM() { BDM_CTPARAM_HEADER(); };
 
+auto set_param = [&](auto* param) {
+  param->simulation_time_step_ = 0.01; // default
+  param->simulation_max_displacement_ = 3.0; // default
+  param->run_mechanical_interactions_ = true;
+  param->export_visualization_ = true;
+  param->visualization_export_interval_ = 100;
+  param->visualize_sim_objects_["Cell"] = {"diameter_"};
+};
+
 inline int Simulate(int argc, const char** argv) {
-  Simulation<> simulation(argc, argv);
+  Simulation<> simulation(argc, argv, set_param);
 
   // Define initial model - in this example: single cell at origin
   auto* rm = simulation.GetResourceManager();
-  rm->push_back(Cell(30));
+
+  rm->template Reserve<Cell>(2);
+
+  Cell cell0({0, 0, 0});
+  cell0.SetDiameter(10);
+  rm->push_back(cell0);
+
+  Cell cell1({2.5, 0, 0});
+  cell1.SetDiameter(10);
+  rm->push_back(cell1);
 
   // Run simulation for one timestep
-  simulation.GetScheduler()->Simulate(1);
+  simulation.GetScheduler()->Simulate(10000);
 
   std::cout << "Simulation completed successfully!" << std::endl;
   return 0;
